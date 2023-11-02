7e6eab0bf65019371c01d053fa71d416ecd6be9d
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-02 21:29:37 +0200

    add debug window logger; add file logger

---
+++ LoggingSystem/LoggingSystem.Library/DebugWindowLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggingSystem.Library
+{
+    public class DebugWindowLogger : ILogger
+    {
+        public void Write(LogLevel level, string message)
+        {
+            System.Diagnostics.Debug.WriteLine($"[{level}] - {message}");
+        }
+    }
+}
---
+++ LoggingSystem/LoggingSystem.Library/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿namespace LoggingSystem.Library
+{
+    public class FileLogger : ILogger
+    {
+        public FileLogger(string directory)
+        {
+            if (directory is null)
+            {
+                throw new ArgumentNullException(nameof(directory));
+            }
+
+            if (string.IsNullOrEmpty(directory))
+            {
+                throw new ArgumentException("Path must not be empty.", nameof(directory));
+            }
+
+            if (!Path.Exists(directory))
+            {
+                throw new DirectoryNotFoundException($"Directory '{directory}' was not found.");
+            }
+
+            Directory = directory;
+        }
+
+        public string Directory { get; }
+
+        public void Write(LogLevel level, string message)
+        {
+            string fileName = $"Log-{DateTime.Today:yyyy-MM-dd}.txt";
+            string pathToLogFile = Path.Combine(Directory, fileName);
+            string content = $"[{level}] - {message}" + Environment.NewLine;
+
+            File.AppendAllText(pathToLogFile, content);
+        }
+    }
+}
